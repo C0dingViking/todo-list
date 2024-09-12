63ef1e093164387509b86f90c1414acb87beefbd
Will <william.ross.lpp.83@gmail.com>
2024-09-12 15:36:27 -0400

    added the min-h-screen class to the main div to replace the custom fill-screen

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,7 +3,7 @@ import Header from "./Header.tsx";
 
 function Root() {
   return (
-    <div className="pl-10 pr-10 pt-2 space-y-5 bg-background fill-screen"> {/*h-screen w-screen*/} 
+    <div className="pl-10 pr-10 pt-2 space-y-5 bg-background min-h-screen"> {/*h-screen w-screen*/} 
       <Header />
       <Outlet />
     </div>
